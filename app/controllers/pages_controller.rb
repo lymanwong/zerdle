682b3eaee33077cad01a16a3df36b1f817116d8d
lyman wong <lymanwong@gmail.com>
2015-07-25 19:19:25 -0700

    added home, mission, login, and signup pages with links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   def home
   end
 
+  def posting
+  end
+
   def about
+  end
+
+  def login
+  end
+
+  def signup
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   #adding home page
-  root 'pages#home'
+  root 'pages#landing'
+
+  #adding posting page
+  get 'posting' => 'pages#posting'
 
   #adding about page
   get 'about' => 'pages#about'
 
+  #adding signup page
+  get 'signup' => 'pages#signup'
 
-
+  #adding login page
+  get 'login' => 'pages#login'
 
 end
